ab4a2bac68c9debde79e033b613776e3a4676c50
Tulio Cafe <tuliocafe@yahoo.com.br>
2021-11-12 00:00:30 -0300

    Aula 11
    Ajustes da aula 11

---
+++ src/main/java/br/com/marcotuliocafe/service/CargoService.java
@@ -0,0 +1,17 @@
+package br.com.marcotuliocafe.service;
+
+import java.util.List;
+import br.com.marcotuliocafe.domain.Cargo;
+
+public interface CargoService {
+	
+	void salvar(Cargo cargo);
+
+	void editar(Cargo cargo);
+	
+	void excluir(Long id);
+	
+	Cargo buscarPorId(Long id);
+	
+	List<Cargo> buscarTodos();
+}
---
+++ src/main/java/br/com/marcotuliocafe/service/DepartamentoServiceImpl.java
@@ -0,0 +1,50 @@
+package br.com.marcotuliocafe.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.marcotuliocafe.dao.DepartamentoDao;
+import br.com.marcotuliocafe.domain.Departamento;
+
+@Service @Transactional(readOnly = false)
+public class DepartamentoServiceImpl implements DepartamentoService {
+
+	@Autowired
+	private DepartamentoDao dao;
+	
+	@Override
+	public void salvar(Departamento departamento) {
+		dao.save(departamento);
+		
+	}
+
+	@Override
+	public void editar(Departamento departamento) {
+		dao.update(departamento);
+		
+	}
+
+	@Override
+	public void excluir(Long id) {
+		dao.delete(id);
+		
+	}
+
+	@Override @Transactional(readOnly = true)
+	public Departamento buscarPorId(Long id) {
+		
+		return dao.findById(id);
+	}
+
+	@Override @Transactional(readOnly = true)
+	public List<Departamento> buscarTodos() {
+	
+		return dao.findAll();
+	}
+
+
+
+}
